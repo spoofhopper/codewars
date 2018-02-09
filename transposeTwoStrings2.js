1627519148f46e1c2e133ba499c0b0217a3561c4
Morgan Holland <morgan.holland@gmail.com>
2018-02-09 09:51:10 -0800

    solved transposeTwoStrings

--- transposeTwoStrings2.js
+++ transposeTwoStrings2.js
@@ -1,23 +1,50 @@
 function transposeTwoStrings(arr) {
 
-  let output = [];
+  let output = "";
+  let new_string = "";
+  let a = "";
+  let b = "";
   let max = 0;
   for (var z = 0; z < arr.length; z++) {
     if (arr[z].length > max) {
       max = arr[z].length
     }
   }
-  let num_words = arr.length;
+  // let num_words = arr.length;
 
-  for (var i = 0; i < num_words; i++) {
-    for (var j = 0; j < max; j++) {
-      output.push(arr[i].charAt[j] + arr[i + 1].charAt[j])
+  for (var i = 0; i < max; i++) {
+    a = arr[0].charAt(i);
+    b = arr[1].charAt(i);
+    if (a == "") {
+      a = " "
+    } else if (b == "") {
+      b = " "
+    };
+    if (i == max - 1) {
+      new_string = a + " " + b;
+      output += new_string;
+    } else {
+      new_string = a + " " + b + "\n";
+      output += new_string;
     }
   }
-
-
 
   console.log(output);
   return output;
 }
+
 transposeTwoStrings(['Hello', 'Worlds']);
+
+/*
+function transposeTwoStrings(arr){
+  var result = [];
+  var len = Math.max(arr[0].length, arr[1].length);
+
+  for (var i = 0; i < len; i++) {
+    var column = (arr[0][i] || " ") + " " + (arr[1][i] || " ");
+    result.push(column);
+  }
+
+  return result.join("\n");
+}
+*/
